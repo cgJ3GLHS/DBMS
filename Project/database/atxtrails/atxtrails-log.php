28f25ed3a0408a1b6b968c53f881033e3722017a
cgJ3GLHS <33527139+cgJ3GLHS@users.noreply.github.com>
2018-12-05 08:57:35 -0600

    Create atxtrails-log.php
    adding a simple function for writing to a log.  Does not include levels or rolling but I don't think we'll have that much traffic :)

---
+++ Project/database/atxtrails/atxtrails-log.php
@@ -0,0 +1,40 @@
+<!-- ################ BEGIN atxtrails-log.php ################ -->
+<?php
+
+#------------------------------------------------------------------------------
+# Function LogMessage
+#
+# Very simple logging function, writes a message to the log
+#
+# IN: message to be written
+#
+# OUT: if write is successful, message is written to log file return true
+#      else return false
+#------------------------------------------------------------------------------
+function LogMessage($message)
+{
+  $logFile="../../atxtrails-logs/atxtrails.log";
+  $date=date('Y-m-d H:i:s.u');
+  
+  $message="[$date] $message\n";
+  
+  if (is_writable($logFile))
+  {
+    if (!file_put_contents($logFile, $message, FILE_APPEND | LOCK_EX))
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  else
+  {
+    return false;
+  }
+
+}
+
+?>
+<!-- ################ END atxtrails-log.php ################ -->
